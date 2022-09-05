888e547d22c894ce08c4cc7cb9321e9d05a1a32e
Ali Can Batur <alicanbatur@gmail.com>
2022-09-05 17:05:09 +0300

    New build is prepared. (0.0.1)

---
+++ release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+######################
+# Convert inputted version from 0.0.1 to 0_0_1
+######################
+version=$1
+UNDERSCOREDVERSION="${version//./_}"
+
+######################
+# Remove all zip files. 
+######################
+rm *.zip
+
+
+######################
+# Zip LICENSE and Framework files. 
+######################
+zip -r Wisho_iOSSDK_$UNDERSCOREDVERSION.zip LICENSE Wisho.framework
+
+
+######################
+# Commit changes, push them and tag the current version.
+######################
+git add *
+git commit -m "New build is prepared. ($1) "
+git tag $1
+git push
+git push origin $1
+
+echo "Now please go github and release the latest tag! Later on you need to push trunk as we described in the doc."
+
+exit 0
